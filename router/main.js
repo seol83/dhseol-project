1d6d0a611e772a68904748728242b2c99254eabc
seol83 <seol83@gmail.com>
2019-08-09 15:59:24 +0900

    코멘트 추가

--- router/main.js
+++ router/main.js
@@ -4,6 +4,7 @@ module.exports = function(app, urlShortener)
     res.render('index');
   });
 
+  //url을 입력받아 줄인 url로 리턴
   app.get('/urlShortener/:orignUrl', function(req, res){
     var originUrl = req.params.orignUrl;
     var shortUrl = urlShortener.getShortUrl(originUrl);
@@ -11,6 +12,7 @@ module.exports = function(app, urlShortener)
     res.json({shortUrl:shortUrl});
   });
 
+  //줄인 url을 이용해 원본 url을 찾아내 301 리다이렉트
   app.get('/:shortUrl', function(req, res){
     let shortUrl = req.params.shortUrl;
     let originUrl = urlShortener.getOriginUrl(shortUrl);
